ce16ec9e969b2f220e799688e1ea49627215d8e3
Veer <40777269+V3RS@users.noreply.github.com>
2021-04-02 14:23:33 -0400

    updated preview video information

--- react-app/src/components/Browse/index.js
+++ react-app/src/components/Browse/index.js
@@ -16,6 +16,24 @@ export default function Browse() {
           controls={false}
           muted={true}
         />
+        <div id="prev__video__info">
+          <div id="prev__video__title__container">
+            <h1>Earth</h1>
+            <h3 id="prev__vid__artist">LilDicky</h3>
+            <p id="prev__vid__description">
+              Written by: David Burd, Benjamin Levin, Magnus August Høiberg,
+              Josh Coleman and Jamil Chammas Produced by: Benny Blanco...
+            </p>
+          </div>
+          <div id="prev__video__btns">
+            <button id="prev__v__play">
+              <i className="ic fas fa-play"></i>Play
+            </button>
+            <button id="prev__v__info">
+              <i className="ic fas fa-info-circle"></i>More Info
+            </button>
+          </div>
+        </div>
       </div>
       <div className="sliders__c">
         <ContentSlider title={"Trending Now"} />
